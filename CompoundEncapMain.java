30415e63ec3464adc14692183bedc5d2819011bf
Harvy <115984523+harvysdlc@users.noreply.github.com>
2022-12-05 15:35:19 +0800

    Add files via upload

---
+++ CompoundEncapMain.java
@@ -0,0 +1,11 @@
+package compoundinterest;
+
+public class CompoundEncapMain {
+
+    public static void main (System[] args) {
+
+        CompoundEncapSystem system = new CompoundEncapSystem();
+
+        system.userInput();  
+    }
+}
