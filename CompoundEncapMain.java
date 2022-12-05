47915cffad32e2ae677f6d4289bf114939a0ef26
Harvy <115984523+harvysdlc@users.noreply.github.com>
2022-12-05 15:54:08 +0800

    Update CompoundEncapMain.java

--- CompoundEncapMain.java
+++ CompoundEncapMain.java
@@ -2,7 +2,7 @@ package compoundinterest;
 
 public class CompoundEncapMain {
 
-    public static void main (System[] args) {
+    public static void main (String[] args) {
 
         CompoundEncapSystem system = new CompoundEncapSystem();
 
